5ec10a2603ac53647de5f4cae8acabe8180f52c0
BalashovM <mi-h-a@mail.ru>
2021-06-26 21:33:18 +0700

    Lesson01 - Finish

---
+++ HW01/script01.js
@@ -0,0 +1,16 @@
+﻿function Script01(blockName){
+    var Tc = prompt("Введите значение температуры в градусах Цельсия: ", "0");
+    if(!isNaN(Tc)){
+        Tc = parseFloat(Tc);
+        var Tf = (9/5) * Tc + 32;
+        alert ("Значение температуры по Фаренгейту: " + Tf);
+
+        //Вывод результата на html-страницу
+        document.getElementById(blockName).style.display = 'block';
+        document.getElementById("text_01_01").innerHTML = ("Значение температуры по Цельсию: " + Tc);
+        document.getElementById("text_01_02").innerHTML = ("Значение температуры по Фаренгейту: " + Tf);
+    }else{
+        alert ("Введены неверные данные!")
+    }
+
+}
---
+++ HW01/script03.js
@@ -0,0 +1,10 @@
+﻿function Script03(blockName){
+    var myNumber = 1000;
+    var myString = "108";
+    var result = myNumber + myString;
+
+    //Вывод результата на html-страницу
+    document.getElementById(blockName).style.display = 'block';
+    document.getElementById("text_03_01").innerHTML = (result);
+    document.getElementById("text_03_02").innerHTML = (typeof(result));
+}
